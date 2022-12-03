295bc68cce42b56e9429fdac9940341bf83af1d3
semanur03 <Semanur.Uyar@Student.HTW-Berlin.de>
2022-12-03 16:25:42 +0100

    skill_answer_questiion_controller_model_route

---
+++ CapCareer/backend/app/routes/question.route.js
@@ -0,0 +1,24 @@
+module.exports = app => {
+    const question = require("../controller/question.controller.js");
+
+    // Create a new Question
+    app.post("/question", question.create);
+
+    // GET all Question
+    app.get("/question", question.findAll);
+
+    // GET one single Question with questionId
+    app.get("/question/id:questionId", question.findOneId);
+
+        // // GET one single Question with questionId
+        // app.get("/question/question/:questionQuestion", question.findOneQuestion);
+
+    // Update one Question with questionId
+    app.put("/question/:questionId", question.update);
+
+    // Delete the Question with questionId
+    app.delete("/question/:questionId", question.delete);
+
+    // Delete all Question
+    app.delete("/question", question.deleteAll);
+};

1a5711a2513167a7c79b0ed2f4673c85ee9237ed
AlexandraIllner <82087641+AlexandraIllner@users.noreply.github.com>
2023-01-07 23:50:31 +0100

    Delete CapCareer directory

--- CapCareer/backend/config/db.config.js
+++
@@ -1,7 +0,0 @@
-// hier wird die (derzeit noch lokale) DB mit dem Backend verbunden, mit den Anmeldedaten des lokalen MySQL
-module.exports = {
-    HOST: "localhost",
-    USER: "root",
-    PASSWORD: "",
-    DB: "capcareer_wizard"
-};

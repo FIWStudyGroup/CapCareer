e479390b55fb52b078ba28cf3771757cec310545
anschmidt01 <antonia.schmidt@student.htw-berlin.de>
2023-01-11 13:12:06 +0100

    Laden von allen Job_posts aus der Datenbank in result commponent

--- frontend/src/app/result/result.component.ts
+++ frontend/src/app/result/result.component.ts
@@ -1,3 +1,5 @@
+import { Job } from '../shared/job';
+import { BackendService } from '../shared/backend.service';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +8,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./result.component.css']
 })
 export class ResultComponent {
+  jobs!: Job[];
 
+  constructor(private bs: BackendService) { }
+
+  ngOnInit(): void {
+    this.readAll();
+    //this.readOneQuestion();
+  }
+  
+  readAll(): void {
+    this.bs.getAllJobs().subscribe( // subscribe-arguments sollen iregdnwie anders angegeben werden, lässt sich auf Anhieb aber nicht auf dieses Problem anwenden
+    (
+      response: Job[]) => {
+              this.jobs = response;
+              console.log(this.jobs);
+              return this.jobs;
+      },
+      error => console.log(error)
+    );
+  }  
 }
